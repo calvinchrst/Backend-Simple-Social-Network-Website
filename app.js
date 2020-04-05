08650ad8d3d009c4cff3b6221895acada7945ecf
Calvin Christian <calvinchrst@gmail.com>
2020-04-05 08:41:35 +0800

    Allow CORS so that public get access to our API

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ const app = express();
 // app.use(bodyparser.urlencoded());   // x-www-form-urleconded <form>
 app.use(bodyparser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type",
+    "Authorization"
+  );
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
