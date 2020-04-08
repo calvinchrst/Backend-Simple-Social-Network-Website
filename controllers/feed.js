12d961c82a43b85b6939dd3954429fa042efd57e
Calvin Christian <calvinchrst@gmail.com>
2020-04-09 07:33:36 +0800

    During delete post request, we also inform all client that post deletion happens

--- controllers/feed.js
+++ controllers/feed.js
@@ -175,6 +175,13 @@ exports.deletePost = async (req, res, next) => {
     const user = await User.findById(post.creator);
     user.posts.pull(postId);
     await user.save();
+
+    // Update all clients on new post
+    io.getIO().emit("posts", {
+      action: "delete",
+      post: postId,
+    });
+
     res.status(200).json({
       message: "Post deleted",
     });
